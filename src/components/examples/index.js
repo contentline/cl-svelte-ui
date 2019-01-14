43f8c001928d6296ad0c7a13e8150aeaa5c677d1
Daniil Khanin <khanin_daniil@mail.ru>
2019-01-14 16:55:50 +0300

    fix storybook

---
+++ src/components/examples/index.js
@@ -0,0 +1,7 @@
+import DefaultCustomBtn from './CustomBtnExample/Default.html'
+import RoundedCustomBtn from './CustomBtnExample/Rounded.html'
+
+export {
+  DefaultCustomBtn,
+  RoundedCustomBtn
+}
