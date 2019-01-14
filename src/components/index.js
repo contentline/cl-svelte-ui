80aeb95f7e50a1a27fba3863281a7d2d94b1af1f
Daniil Khanin <khanin_daniil@mail.ru>
2019-01-14 14:39:59 +0300

    change directory

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import CustomButton from './CustomButton.html'
+import DefaultCustomBtn from './examples/CustomBtnExample/Default.html'
+import RoundedCustomBtn from './examples/CustomBtnExample/Rounded.html'
+
+export {
+  RoundedCustomBtn,
+  DefaultCustomBtn,
+  CustomButton
+}
