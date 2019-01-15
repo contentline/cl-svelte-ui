beb87dbda3fcc34cad499ad2c3687977d4cf7d44
Daniil Khanin <khanin_daniil@mail.ru>
2019-01-15 13:49:08 +0300

    implement

--- src/stories/index.js
+++ src/stories/index.js
@@ -29,7 +29,7 @@ storiesOf('CustomButton', module)
     }
   }))
 
-storiesOf.add('Swiper', module)
+storiesOf('Swiper', module)
   .add('default Swiper', () => ({
     Component: DefaultSwiper
   }))
