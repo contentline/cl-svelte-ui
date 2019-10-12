073f098f6837c05027e8be6976118bbb0ac3cf6e
Daniil Khanin <khanin_daniil@mail.ru>
2019-10-13 00:34:23 +0300

    implement select custom scroll

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,12 +1,16 @@
 <script>
-  import { onMount, onDestroy, createEventDispatcher }  from 'svelte'
+  import { onMount, onDestroy, createEventDispatcher, afterUpdate }  from 'svelte'
   import '../css/nice-select/nice-select.css'
-
-  onMount(() => window.addEventListener('click', closeClickHandle))
+  import SimpleBar from 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+  import 'simplebar/dist/simplebar.css';
+  onMount(() => {
+      window.addEventListener('click', closeClickHandle)
+      //console.log('listElem', listElem)
+      bar = new SimpleBar(listElem)
+  })
   onDestroy(() => typeof window !== 'undefined' && window.removeEventListener('click', closeClickHandle))
-
   const dispatch = createEventDispatcher()
-  let selectWrapper
+  let selectWrapper, listElem, bar
   export let data = [], name='', nullItem = { value: '', text: '---' }, selectedItem = nullItem, required = true
 
   const defaultSelectedItem = selectedItem
@@ -44,16 +48,42 @@
     {/each}
   </select>
   <span class="current">{selectedItem.text}</span>
-  <ul class="list">
-    {#each resultData as optionItem}
-      <li
-        on:click={e => itemClickHandle(e, optionItem)}
-        class="option"
-        class:selected={selectedItem.value === optionItem.value}
-        class:disabled={optionItem.disabled}
-      >{optionItem.text}</li>
-    {/each}
-  </ul>
+    <ul class="list">
+        <div bind:this={listElem}  style='max-height: 300px;'>
+            <div class="simplebar-wrapper">
+                <div class="simplebar-height-auto-observer-wrapper">
+                    <div class="simplebar-height-auto-observer" />
+                </div>
+                <div class="simplebar-mask">
+                    <div class="simplebar-offset">
+                        <div class='simplebar-content-wrapper'>
+                            <div class="simplebar-content">
+                                <div>
+                                  {#each resultData as optionItem}
+                                    <li
+                                            on:click={e => itemClickHandle(e, optionItem)}
+                                    class="option"
+                                    class:selected={selectedItem.value === optionItem.value}
+                                    class:disabled={optionItem.disabled}
+                                    >{optionItem.text}</li>
+                                  {/each}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="simplebar-placeholder" />
+            </div>
+            <div className="simplebar-track simplebar-horizontal">
+                <div className="simplebar-scrollbar" />
+            </div>
+            <div className="simplebar-track simplebar-vertical">
+                <div className="simplebar-scrollbar" />
+            </div>
+        </div>
+
+    </ul>
+
 </div>
 
 <style>
